6210a15985a9d4fe24397063f067e30155d2282d
souhirfar <souhir_farjallah@live.fr>
2023-11-04 00:21:40 +0100

    components1

---
+++ my-react-poke/src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+  return (
+    <figure className="card">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Bulbasaur"
+        className="card-img"
+      />
+      <figcaption>"Bulbasaur"</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
