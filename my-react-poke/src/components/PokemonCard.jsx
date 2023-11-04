8bbf153efa66484557bc738f9acdab3358dfe8d3
souhirfar <souhir_farjallah@live.fr>
2023-11-04 09:46:27 +0100

    components.2

--- my-react-poke/src/components/PokemonCard.jsx
+++ my-react-poke/src/components/PokemonCard.jsx
@@ -1,12 +1,23 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  let pokemon = pokemonList[1];
+
   return (
     <figure className="card">
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-        className="card-img"
-      />
-      <figcaption>"Bulbasaur"</figcaption>
+      {pokemon.imgSrc ? 
+      <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />:
+      <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
