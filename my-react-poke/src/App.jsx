d9da402912864366bcb1235cdeff844bb0e1f721
souhirfar <souhir_farjallah@live.fr>
2023-11-04 14:41:10 +0100

    props.1

--- my-react-poke/src/App.jsx
+++ my-react-poke/src/App.jsx
@@ -1,9 +1,20 @@
 import PokemonCard from "./components/PokemonCard";
-import "./styles/App.css"
+import "./styles/App.css";
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
--- my-react-poke/src/components/PokemonCard.jsx
+++ my-react-poke/src/components/PokemonCard.jsx
@@ -1,22 +1,12 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  let pokemon = pokemonList[1];
+function PokemonCard({pokemon}) {
 
   return (
     <figure className="card">
-      {pokemon.imgSrc ? 
-      <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />:
-      <p>???</p>}
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
